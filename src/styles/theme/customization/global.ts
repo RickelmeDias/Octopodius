a99fbf33c50ce8bec6e012362702b1c57068b7b8
Rickelme Dias <rickelmegadias@gmail.com>
2023-03-11 00:53:24 -0300

    Chakra UI and custom theme added

---
+++ src/styles/theme/customization/global.ts
@@ -0,0 +1,17 @@
+const global = {
+  '*': {
+    boxSizing: 'border-box',
+  },
+  body: {
+    bg: 'gray.100',
+    color: 'gray.700',
+    margin: 0,
+    padding: 0,
+  },
+  html: {
+    margin: 0,
+    padding: 0,
+  },
+}
+
+export default global
